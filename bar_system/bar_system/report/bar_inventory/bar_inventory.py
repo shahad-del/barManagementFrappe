d1c238d7a4e71ffedccd0fe4fc592cc2ac016ffb
shahad-del <shahad.sumann@gmail.com>
2023-04-17 16:32:48 +0545

    using placeholder in query

--- bar_system/bar_system/report/bar_inventory/bar_inventory.py
+++ bar_system/bar_system/report/bar_inventory/bar_inventory.py
@@ -23,15 +23,18 @@ filters={
     },                          
 fields=["name","brand"])
 frappe.msgprint(f'{main_bottle_name}')
-loose_bottle_name =frappe.db.sql("""SELECT item_code from `tabItem` where item_code  LIKE '%loose%' """);
-# frappe.msgprint(f'{loose_bottle_name}')
-k = [item[0] for item in loose_bottle_name]
-frappe.msgprint(f'{k}')
+# loose_bottle_name =frappe.db.sql("""SELECT item_code from `tabItem` where item_code  LIKE '%loose%' """);
+# # frappe.msgprint(f'{loose_bottle_name}')
+# k = [item[0] for item in loose_bottle_name]
+# frappe.msgprint(f'{k}')
 for item in main_bottle_name:
    
     # brand = frappe.db.get_value("Item",item.name,"item_code")
+    
     brand_name = item.brand
-    frappe.msgprint(f'{brand_name}')  
+    main_and_loose_bottle = frappe.db.sql("""select item_code,actual_qty  FROM `tabBin` where item_code like %(brand_name)s and actual_qty > 0""",{'brand_name': f'{brand_name}%'});
+
+    frappe.msgprint(f'{main_and_loose_bottle}')  
 def get_columns():
     return[
 		"Brand:Data:200",
